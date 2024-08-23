6755762d6a240f492ec43943845b1fd118b65fe5
mikhawa <michaeljpitz@gmail.com>
2024-08-23 11:31:18 +0200

    php bin/console make:controller BlogController

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
